1ad549765cb63e2397cd0c0167b929604008f314
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2017-03-26 23:28:42 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyCdM8qb072k7D3dx-W_5K3BraPv-Hg3XF0',
+      authDomain: 'manager-e726e.firebaseapp.com',
+      databaseURL: 'https://manager-e726e.firebaseio.com',
+      storageBucket: 'manager-e726e.appspot.com',
+      messagingSenderId: '881006462938'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>hello</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
